5d78c7bdb3b34820bd29e490e738593684b3e6b5
Damien Pinna <damien.pinna@gmail.com>
2020-07-08 11:57:53 +0200

    création d'une application Express

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+ });
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send('Votre requête a bien été reçue !');
+  next();
+});
+
+module.exports = app;
