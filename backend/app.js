a7597b926f66afb65050b7e88588ab2f94dbd3a4
Damien Pinna <damien.pinna@gmail.com>
2020-07-09 10:38:59 +0200

    ajout d'un routeur

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 const db = require('./config-db');
-const auth = require('./config-token');
-const User = require('./models/user');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
+//Connexion à la base de données MongoDB Atlas.
 mongoose.connect(`mongodb+srv://${db.users}:${db.password}@flachibou-34bhw.gcp.mongodb.net/${db.name}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
   .then(() => console.log('Connexion à MongoDB réussie.'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+//CORS
 app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -23,42 +22,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/auth/signup', async (req, res) => {
-   try {
-      const hash = await bcrypt.hash(req.body.password, 10);
-      const user = new User({
-         email: req.body.email,
-         password: hash
-      });
-      user.save()
-         .then(() => res.status(201).json({message: 'Utilisateur enregistré.'}))
-         .catch(error => res.status(400).json({error}));
-   } catch (error) {
-      res.status(500).json({error});
-   }
-});
-
-app.post('/api/auth/login', async (req, res) => {
-   try {
-      const user = await User.findOne({email: req.body.email});
-      if(!user) return res.status(401).json({error: 'Utilisateur non trouvé !'});
-      try {
-         const valid = await bcrypt.compare(req.body.password, user.password);
-         if(!valid) return res.status(401).json({error: 'Mot de passe incorrect.'});
-         res.status(200).json({
-            userId: user._id,
-            token: jwt.sign(
-               {userId: user._id},
-               `${auth.secret}`,
-               {expiresIn: '24h'}
-            )
-         });
-      } catch (error) {
-         res.status(500).json({error});
-      };
-   } catch (error) {
-      res.status(500).json({error});
-   };
-});
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
