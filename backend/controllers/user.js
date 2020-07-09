9bb73fe9ecbc52754979c5b74783cdf5d5fd8371
Damien Pinna <damien.pinna@gmail.com>
2020-07-09 11:06:21 +0200

    ajout d'un contrôleur

---
+++ backend/controllers/user.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+const auth = require('../config-token');
+
+exports.signup = async (req, res) => {
+   try {
+      const hash = await bcrypt.hash(req.body.password, 10);
+      const user = new User({
+         email: req.body.email,
+         password: hash
+      });
+      user.save()
+         .then(() => res.status(201).json({message: 'Utilisateur enregistré.'}))
+         .catch(error => res.status(400).json({error}));
+   } catch (error) {
+      res.status(500).json({error});
+   }
+};
+
+exports.login = async (req, res) => {
+   try {
+      const user = await User.findOne({email: req.body.email});
+      if(!user) return res.status(401).json({error: 'Utilisateur non trouvé !'});
+      try {
+         const valid = await bcrypt.compare(req.body.password, user.password);
+         if(!valid) return res.status(401).json({error: 'Mot de passe incorrect.'});
+         res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+               {userId: user._id},
+               `${auth.secret}`,
+               {expiresIn: '24h'}
+            )
+         });
+      } catch (error) {
+         res.status(500).json({error});
+      };
+   } catch (error) {
+      res.status(500).json({error});
+   };
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,47 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 
-const User = require('../models/user');
-const auth = require('../config-token');
+const userController = require('../controllers/user')
 
-router.post('/signup', async (req, res) => {
-   try {
-      const hash = await bcrypt.hash(req.body.password, 10);
-      const user = new User({
-         email: req.body.email,
-         password: hash
-      });
-      user.save()
-         .then(() => res.status(201).json({message: 'Utilisateur enregistré.'}))
-         .catch(error => res.status(400).json({error}));
-   } catch (error) {
-      res.status(500).json({error});
-   }
-});
 
-router.post('/login', async (req, res) => {
-   try {
-      const user = await User.findOne({email: req.body.email});
-      if(!user) return res.status(401).json({error: 'Utilisateur non trouvé !'});
-      try {
-         const valid = await bcrypt.compare(req.body.password, user.password);
-         if(!valid) return res.status(401).json({error: 'Mot de passe incorrect.'});
-         res.status(200).json({
-            userId: user._id,
-            token: jwt.sign(
-               {userId: user._id},
-               `${auth.secret}`,
-               {expiresIn: '24h'}
-            )
-         });
-      } catch (error) {
-         res.status(500).json({error});
-      };
-   } catch (error) {
-      res.status(500).json({error});
-   };
-});
+router.post('/signup', userController.signup);
+router.post('/login', userController.login);
 
 module.exports = router;
