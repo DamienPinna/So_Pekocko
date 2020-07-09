de1dc78e86cd02747546f31c37b396dd5834c468
Damien Pinna <damien.pinna@gmail.com>
2020-07-09 17:33:21 +0200

    ajout d'un filtre pour n'accepter que les images jpg, jpeg et png

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,20 +1,28 @@
 const multer = require('multer');
 
 const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
+   'image/jpg': 'jpg',
+   'image/jpeg': 'jpg',
+   'image/png': 'png'
 };
 
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
+   destination: (req, file, callback) => {
+      callback(null, 'images');
+   },
+   filename: (req, file, callback) => {
+      const name = file.originalname.split(' ').join('_');
+      const extension = MIME_TYPES[file.mimetype];
+      callback(null, name + Date.now() + '.' + extension);
+   }
 });
 
-module.exports = multer({storage: storage}).single('image');
+const fileFilter = (req, file, callback) => {
+   if(file.mimetype === "image/jpg" || file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      callback(null, true);
+   } else {
+      callback(new Error("le format de l'image n'est pas accepté"), false);
+   };
+};
+
+module.exports = multer({storage: storage, fileFilter: fileFilter}).single('image');
