39e422b7076ee45c469478cc6bc3c6a783ea30ef
Damien Pinna <damien.pinna@gmail.com>
2020-07-10 11:08:57 +0200

    suppression sur le serveur du fichier de l'image supprimée

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs');
 
 exports.createSauce = (req, res) => {
    const sauceObject = JSON.parse(req.body.sauce);
@@ -14,19 +15,27 @@ exports.createSauce = (req, res) => {
 
 exports.modifySauce = (req, res) => {
    const sauceObject = req.file ? 
-   { 
+   {
       ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-      } : { ...req.body };
+   } : { ...req.body };
    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
       .catch(error => res.status(400).json({error}));
 };
 
-exports.deleteSauce = (req, res) => {
-   Sauce.deleteOne({_id: req.params.id})
-      .then(() => res.status(200).json({ message: 'Sauce supprimée'}))
-      .catch(error => res.status(400).json({error}));
+exports.deleteSauce = async (req, res) => {
+   try {
+      const sauce = await Sauce.findOne({_id: req.params.id});
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+         Sauce.deleteOne({_id: req.params.id})
+            .then(() => res.status(200).json({ message: 'Sauce supprimée'}))
+            .catch(error => res.status(400).json({error}));
+      });
+   } catch (error) {
+      res.status(500).json({error});
+   };
 };
 
 exports.getOneSauce = (req, res) => {
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,7 +16,7 @@ exports.signup = async (req, res) => {
          .catch(error => res.status(400).json({error}));
    } catch (error) {
       res.status(500).json({error});
-   }
+   };
 };
 
 exports.login = async (req, res) => {
