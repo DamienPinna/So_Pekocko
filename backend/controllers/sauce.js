f3b1b66b22f0e4606616905a320ca62dd572223f
Damien Pinna <damien.pinna@gmail.com>
2020-07-12 16:14:17 +0200

    ajout du controle si l'utilisateur aime ou n'aime pas déjà la sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -13,18 +13,27 @@ exports.manageLikeAndDislike = async (req, res) => {
 
    switch (req.body.like) {
       //L'utilisateur aime la sauce.
-      case 1: 
-         likeAndDislikeUpdate.likes += 1;
-         likeAndDislikeUpdate.userLiked.push(req.body.userId);
-         console.log(likeAndDislikeUpdate);
+      case 1:
+         const indexUserIdInUserLiked = sauce.userLiked.findIndex(userIdInUserLiked => userIdInUserLiked === req.body.userId);
+         if (indexUserIdInUserLiked === -1) {
+            likeAndDislikeUpdate.likes += 1;
+            likeAndDislikeUpdate.userLiked.push(req.body.userId);
+         } else {
+            res.status(200).json({ message: "L'utilisateur aime déjà la sauce."});
+         };
          break;
       //L'utilisateur annule ce qu'il aime ou n'aime pas.
       case 0: console.log(0);
          break;
       //L'utilisateur n'aime pas la sauce.
       case -1:
-         likeAndDislikeUpdate.dislikes += 1;
-         likeAndDislikeUpdate.userDisliked.push(req.body.userId);
+         const indexUserIdInUserDisliked = sauce.userDisliked.findIndex(userIdInUserDisliked => userIdInUserDisliked === req.body.userId);
+         if (indexUserIdInUserDisliked === -1) {
+            likeAndDislikeUpdate.dislikes += 1;
+            likeAndDislikeUpdate.userDisliked.push(req.body.userId);
+         } else {
+            res.status(200).json({ message: "L'utilisateur n'aime déjà pas la sauce."});
+         };
          break;
       default: console.log('Erreur dans la requête !');
    }
