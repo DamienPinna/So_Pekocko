abbefa87f4cd8b67f1613aee8f620feeb6c1dd08
Damien Pinna <damien.pinna@gmail.com>
2020-07-19 17:50:45 +0200

    ajout du module helmet permettant de configurer les en-têtes HTTP dans le but de protéger l'application de certaintes vulnérabilités

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const path = require('path');
 
 const userRoutes = require('./routes/user');
@@ -23,7 +24,7 @@ app.use((req, res, next) => {
  });
 
 app.use(bodyParser.json());
-// mettre ic helmet
+app.use(helmet());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
