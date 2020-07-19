70425e8364c3b4c55d514f10d9dbf403997ebc8b
Damien Pinna <damien.pinna@gmail.com>
2020-07-19 17:21:23 +0200

    à l'inscription, controle de la forme de l'adresse mail pour se prémunir de la faille CRLF + obligation de renseigner un mot de passe entre 8 et 64 caractères comme le préconise l'OWASP

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -8,17 +8,25 @@ const User = require('../models/user');
  * Créé un utilisateur et chiffre son mot de passe.
  */
 exports.signup = async (req, res) => {
-   try {
-      const hash = await bcrypt.hash(req.body.password, 10);
-      const user = new User({
-         email: req.body.email,
-         password: hash
-      });
-      user.save()
-         .then(() => res.status(201).json({message: 'Utilisateur enregistré.'}))
-         .catch(error => res.status(400).json({error}));
-   } catch (error) {
-      res.status(500).json({error});
+   const email = req.body.email;
+   const password = req.body.password;
+   const regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+
+   if (regex.test(email) && password.length > 7 && password.length < 65) {
+      try {
+         const hash = await bcrypt.hash(password, 10);
+         const user = new User({
+            email: email,
+            password: hash
+         });
+         user.save()
+            .then(() => res.status(201).json({message: 'Utilisateur enregistré.'}))
+            .catch(error => res.status(400).json({error}));
+      } catch (error) {
+         res.status(500).json({error});
+      };
+   } else {
+      res.status(500).json({message: 'Mot de passe inférieur a 8 caractères et/ou adresse mail invalide. '})
    };
 };
 
--- backend/middleware/xss-auth.js
+++ backend/middleware/xss-auth.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
    const userObject = req.body;
-   const regex = /<|>|"|&/
+   const regex = /<|>|"|&/;
    isDangerous = regex.test(userObject.email) || regex.test(userObject.password);
    if (isDangerous) {
       res.status(500).json({message: 'Attaque XSS suspectée.'})
